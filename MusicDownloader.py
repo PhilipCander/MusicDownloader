bc3e56ddfc0a829e3057e4a81a87280a635e64a5
PhilipCander <55022110+PhilipCander@users.noreply.github.com>
2020-01-08 01:53:20 +0100

    Create MusicDownloader.py
    Ready to use

---
+++ MusicDownloader.py
@@ -0,0 +1,12 @@
+from youtube_dl import YoutubeDL
+
+print("Kopiere den Youtube Link")
+
+try:
+    link = input(": ")
+    ydl_opts = {}
+    ydl = YoutubeDL(ydl_opts)
+    ydl.download([link])
+except:
+    print("Irgendetwas ist schiefgelaufen, versuchs noch mal...")
+print("Fertig...")
